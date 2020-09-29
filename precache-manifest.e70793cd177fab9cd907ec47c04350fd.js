d1dbbabaf10b20d313ade7673e2e2963bd9aa4e3
Diegoes7 <dimitar.i.dimitrov77@gmail.com>
2020-09-29 17:17:56 +0300

    Updates

---
+++ precache-manifest.e70793cd177fab9cd907ec47c04350fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46913e52b0dc7bddb9581b92bcdaba93",
+    "url": "/mosters-collection/index.html"
+  },
+  {
+    "revision": "3f63a302c3ec261568a8",
+    "url": "/mosters-collection/static/css/main.2c1865e8.chunk.css"
+  },
+  {
+    "revision": "8d04a2400388f7521cee",
+    "url": "/mosters-collection/static/js/2.bf65a8a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mosters-collection/static/js/2.bf65a8a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f63a302c3ec261568a8",
+    "url": "/mosters-collection/static/js/main.0657b9cc.chunk.js"
+  },
+  {
+    "revision": "123f9723d9faa73fac77",
+    "url": "/mosters-collection/static/js/runtime-main.35a82896.js"
+  }
+]);
